ea5c2826ef467e470e321b9eb3cae548f899abdd
girage <45748977+girage@users.noreply.github.com>
2022-03-01 01:26:21 +0700

    upgrade API, add search by params
    mongo $regex
    $regex: name, $options: 'i'

--- controller/products.js
+++ controller/products.js
@@ -5,15 +5,35 @@ const Product = require('../models/products');
 
 
 const getAllProductsStatic = async (req, res) => {
+  const search = 'ab';
+
+
   const products = await Product.find({
-    name: 'vase table',
+    name: { $regex: search, $options: 'i' },
   });
   res.status(200).json({ msg: products, nbHits: products.length, });
 }
 
 const getAllProducts = async (req, res) => {
-  const products = await Product.find(req.query);
-  console.log(req.query);
+  const { featured, company, name } = req.query;
+  const queryObject = {};
+
+  if (company) {
+    queryObject.company = company;
+  }
+
+  if (featured) {
+    queryObject.featured = featured === 'true' ? true : false;
+  }
+
+  if (name) {
+    // queryObject.name = name;
+    queryObject.name = { $regex: name, $options: 'i' };
+  }
+
+  console.log(queryObject);
+  const products = await Product.find(queryObject);
+
   res.status(200).json({ msg: products, nbHits: products.length, });
 }
 
